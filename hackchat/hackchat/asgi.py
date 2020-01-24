37670e67ea16ac72e88b7fa0e390bbfba7c7accc
Reece Berens <rberens@ksu.edu>
2020-01-24 14:40:41 -0600

    Add gitignore and initial django project

---
+++ hackchat/hackchat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hackchat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackchat.settings')
+
+application = get_asgi_application()
