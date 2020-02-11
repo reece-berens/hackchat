d4c3a17241796ca16b87dd4b709786d51ad1c4e2
Reece Berens <rberens@ksu.edu>
2020-02-11 13:48:50 -0600

    More work on initial mock-up of Vue page

--- Vue Examples/messageBoxTestJS.js
+++ Vue Examples/messageBoxTestJS.js
@@ -1,21 +1,38 @@
+var userIsOrganizer = false;
+
 var app = new Vue({
 	el: '#app',
 	data: {
-		todoText: '',
-		todos: [
-			{text: 'Vue.js', done: true},
-			{text: 'Vue.js', done: false},
-		]
+		messageText: '',
+		messages: [
+			{text: 'MSG 1', author: "You", isOrg: false},
+			{text: 'Message 2', author: "Bill The Organizer", isOrg: true},
+		],
+		orgStyleObject: {
+			fontSize: '16pt',
+			color: 'blue'
+		},
+		normalStyleObject: {
+			fontSize: '14pt',
+			color: 'black'
+		}
 	},
 	methods: {
-		addTodo: function() {
-			var newTodo = this.todoText.trim();
-			if (!newTodo) {return;}
-			this.todos.push(
-				{text: newTodo}
+		addMessage: function() {
+			var newMessage = this.msgText.trim();
+			if (!newMessage) {return;}
+			this.messages.push(
+				{text: newMessage, author: "You", isOrg: userIsOrganizer}
 			);
-			this.todoText = '';
+			this.msgText = '';
 		}
 	}
+});
+
+$("#outsideBtn").click(function () {
+	console.log("ON CLICK");
+	app.messages.push(
+		{text: "Other User Text", author: "Other User", isOrg: false}
+	);
 });
 
