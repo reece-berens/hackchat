c1d05f832c0efd29d60cec08366ce6c7ea1979f6
Reece Berens <rberens@ksu.edu>
2020-02-18 16:40:38 -0600

    Add async version of ChatConsumer

--- channelstest/chat/consumers.py
+++ channelstest/chat/consumers.py
@@ -1,7 +1,9 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 import json
 from asgiref.sync import async_to_sync
 
+"""
+#SYNCHRONOUS VERSION
 class ChatConsumer(WebsocketConsumer):
 	def connect(self):
 		self.room_name = self.scope['url_route']['kwargs']['roomName']
@@ -32,16 +34,53 @@ class ChatConsumer(WebsocketConsumer):
 				'message': message
 			}
 		)
-		"""
-		self.send(text_data = json.dumps({
-			'message': message
-		}))
-		"""
+		
+		#self.send(text_data = json.dumps({
+		#	'message': message
+		#}))
+		
 
 	#Receive message from room group
 	def chat_message(self, event):
 		message = event['message']
 		#Send message to WebSocket
 		self.send(text_data=json.dumps({
+			'message': message
+		}))
+"""
+
+class ChatConsumer(AsyncWebsocketConsumer):
+	async def connect(self):
+		self.room_name = self.scope['url_route']['kwargs']['roomName']
+		self.room_group_name = 'chat_%s' % self.room_name
+		#Join room group
+		await self.channel_layer.group_add(
+			self.room_group_name,
+			self.channel_name
+		)
+		await self.accept()
+
+	async def disconnect(self, closeCode):
+		await self.channel_layer.group_discard(
+			self.room_group_name,
+			self.channel_name
+		)
+
+	async def receive(self, text_data):
+		textDataJson = json.loads(text_data)
+		message = textDataJson['message']
+
+		#Send message to room group
+		await self.channel_layer.group_send(
+			self.room_group_name,
+			{
+				'type': 'chat_message',
+				'message': message
+			}
+		)
+
+	async def chat_message(self, event):
+		message = event['message']
+		await self.send(text_data=json.dumps({
 			'message': message
 		}))
