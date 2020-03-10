ccd2cf266fcf99c4cb25c114c6066622e238e619
Reece Berens <rberens@ksu.edu>
2020-03-09 20:09:17 -0500

    Put isOrganizer field in MLHUser, update my superuser to have it

--- hackchat/mlhAuth/models.py
+++ hackchat/mlhAuth/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class MLHUser(AbstractUser):
 	updated_at = models.DateTimeField(auto_now=True, help_text='The last time this record was updated.')
+	isOrganizer = models.BooleanField(default=False)
 
 	def __str__(self):
 		return "{}".format(self.email)
