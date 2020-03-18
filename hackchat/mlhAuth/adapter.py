45a7a2dd69a5b026d3e3f83e38fa878615b20b69
Reece Berens <rberens@ksu.edu>
2020-03-18 15:57:27 -0500

    Add logout button and add/update ChannelPermission objects for user logging in

--- hackchat/mlhAuth/adapter.py
+++ hackchat/mlhAuth/adapter.py
@@ -1,24 +1,59 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from django.urls import reverse
+from django.db.models.query import QuerySet, EmptyQuerySet
 
 from mlhAuth.models import MLHUser
+from chat.models import Channel, ChannelPermissions
 
 
 class AccountAdapter(DefaultAccountAdapter):
     def get_login_redirect_url(self, request):
-        #url = super(AccountAdapter, self).get_login_redirect_url(request)
         url = '/callback'
         print('hi')
+        print(request.path)
         if 'login/callback' in request.path:
-            try:
-                print(request)
-                print(request.user)
-                profile = MLHUser.objects.get(email=request.user)
-            except MLHUser.DoesNotExist:
-                print("Does not exist")
-                profile = None
-            if profile is None:
-                print("Inside profile is none in adapter")
-                url = reverse('registration:register')
+            print(request)
+            print(request.user)
+            profile = MLHUser.objects.get(email=request.user)
+            print(profile)
+            allChannels = Channel.objects.all()
+            print(type(allChannels))
+            for i in allChannels:
+                try:
+                    cpObject = ChannelPermissions.objects.filter(channelID=i.id).filter(participantID=profile.id)
+                    print("Type of cpObject is {}".format(type(cpObject)))
+                    print(cpObject)
+                    if not cpObject:
+                        #We need to create a ChannelPermission object
+                        print("Empty query set")
+                        newPermission = ChannelPermissions()
+                        newPermission.channelID = i
+                        newPermission.participantID = profile
+                        if (profile.isOrganizer == True):
+                            newPermission.permissionStatus = 3
+                        else:
+                            #Normal user
+                            if (i.organizerOnly == True):
+                                newPermission.permissionStatus = 0
+                            else:
+                                newPermission.permissionStatus = 2
+                        newPermission.save()
+                    else:
+                        print("Not empty query set")
+                        #Update the current one
+                        newPermission = cpObject[0]
+                        newPermission.channelID = i
+                        newPermission.participantID = profile
+                        if (profile.isOrganizer == True):
+                            newPermission.permissionStatus = 3
+                        else:
+                            #Normal user
+                            if (i.organizerOnly == True):
+                                newPermission.permissionStatus = 0
+                            else:
+                                newPermission.permissionStatus = 2
+                        newPermission.save()
+                except Exception as e:
+                    print(e)
         print('callback-' + url)
         return url
