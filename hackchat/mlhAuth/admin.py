bf30e84cbfe9c01aea1135990a2645fb7f5127da
Reece Berens <rberens@ksu.edu>
2020-04-27 20:47:03 -0500

    Automatically change ChannelPermission status when updating isOrganizer property of user in admin page

--- hackchat/mlhAuth/admin.py
+++ hackchat/mlhAuth/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import MLHUser
+from chat.models import Channel, ChannelPermissions
 
 # Register your models here.
 
@@ -14,5 +15,18 @@ class MLHUserAdmin(BaseUserAdmin):
 		(None, {'fields': ('permanentMute',)}),
 		(None, {'fields': ('muteInstances',)}),
 	)
+
+	def save_model(self, request, user, form, change):
+		super().save_model(request, user, form, change)
+		cpList = ChannelPermissions.objects.filter(participantID=user)
+		if (user.isOrganizer == True):
+			#Give the user organizer permissions on all channels
+			for cp in cpList:
+				cp.permissionStatus = 3
+				cp.save()
+		else:
+			for cp in cpList:
+				cp.permissionStatus = cp.channelID.defaultPermissionStatus
+				cp.save()
 
 admin.site.register(MLHUser, MLHUserAdmin)
